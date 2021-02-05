49fe5956b3eb2f28a193a389b804ac4f446dd0fc
janarthan s <janathanreddy@gmail.com>
2021-02-05 15:17:27 +0530

    StoryBoard Created

---
+++ TableViewInsideCollectionViewCell/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  TableViewInsideCollectionViewCell
+//
+//  Created by Janarthan Subburaj on 05/02/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var ButtonAction: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ TableViewInsideCollectionViewCell/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  TableViewInsideCollectionViewCell
+//
+//  Created by Janarthan Subburaj on 05/02/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
